903ebbbccc0057f19ddfe3d12d0f4b99766c5f96
Sam Bryan <sambena@gmail.com>
2012-08-31 16:30:22 -0600

    First Build
    There are several bugs, needs love

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 130);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1071, 589);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sound On";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(263, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Clear Page";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 41);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Change Color";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(91, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(77, 41);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Say It";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(345, 1);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 40);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Find Mode Off";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 73);
+            this.label1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(420, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 73);
+            this.label2.TabIndex = 9;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(426, 0);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 40);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Word Mode Off";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(508, 1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Word Lengh";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(508, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(66, 21);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.Text = "5";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 741);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Speak and Spell";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.AudioFormat;
+using System.IO;
+using System.Media;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        private string[] sound = new string[26];
+        private SoundPlayer player;
+        private bool toggleSound = true;
+        private bool findMode = false;
+        private bool wordMode = false;
+        private bool found = false;
+        private SpeechSynthesizer speak = new SpeechSynthesizer();
+        private char letter;
+        private int wordLenght = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            int index = 97;
+            for(int i = 0; i < 26; i++)
+            {
+                sound[i] = "Music\\" + (char)(i+index) + ".wav";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            string textToSpeak;
+            int last = 0;
+            textToSpeak = textBox1.Text.ToLower();
+            last = textToSpeak.Length - 1;
+
+            if (last >= 0)
+            {
+                var location = textToSpeak[last];
+
+                if (location >= 97 && location <= 122)
+                {
+                    player = new SoundPlayer();
+
+                    player.SoundLocation = sound[location - 97].ToString();
+
+                    if (toggleSound)
+                    {
+                        player.Play();
+                    }
+                }
+            }
+
+            if (findMode)
+            {
+                var answer = textBox1.Text;
+                if (answer == letter.ToString())
+                {
+                    found = true;
+                }
+
+                if (found)
+                {
+                    speak.Speak("good job you found it");
+                    find();
+                }
+                else if(!found && textBox1.Text != "")
+                {
+                    speak.Speak("That is not correct");
+                }
+
+                //textBox1.Text = null;
+            }
+
+            if (wordMode)
+            {
+                if (wordLenght == textBox1.Text.Length)
+                {
+                    player.Stop();
+                    speak.Speak(textBox1.Text);
+                    textBox1.Text = null;
+                    textBox1.Focus();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Sound On")
+            {
+                button1.Text = "Sound Off";
+                toggleSound = false;
+            }
+            else
+            {
+                button1.Text = "Sound On";
+                toggleSound = true;
+            }
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            textBox1.Focus();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Show the color dialog.
+            DialogResult result = colorDialog1.ShowDialog();
+            // See if user pressed ok.
+            if (result == DialogResult.OK)
+            {
+                // Set form background to the selected color.
+                textBox1.ForeColor = colorDialog1.Color;
+            }
+            textBox1.Focus();
+        }
+
+        //play text
+        private void button4_Click(object sender, EventArgs e)
+        {
+            speak.Volume = 100;
+            speak.Rate = -3;
+            if (toggleSound)
+            {
+                if (textBox1.Text.Length < 32)
+                {
+                    speak.Speak(textBox1.Text);
+                }
+                else
+                {
+                    speak.Speak("good job typing");
+                }
+                textBox1.Text = null;
+                textBox1.Focus();
+            }
+        }
+
+        //find the letter
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.Text == "Find Mode On")
+            {
+                button5.Text = "Find Mode Off";
+                findMode = false;
+            }
+            else
+            {
+                textBox1.Text = null;
+                button5.Text = "Find Mode On";
+                findMode = true;
+            }
+
+            find();
+        }
+
+        void find()
+        {
+            textBox1.Text = null;
+            found = false;
+
+            Random rand = new Random((int)DateTime.Now.Ticks);
+            var randLetter = rand.Next(97, 122);
+            letter = (char)randLetter;
+
+            string question = "Find the letter ";
+
+            label1.Text = question;
+            label2.Text = letter.ToString();
+
+            speak.Speak(label1.Text);
+            player = new SoundPlayer();
+            player.SoundLocation = sound[randLetter - 97].ToString();
+            player.Play();
+
+            textBox1.Focus();
+        }
+
+        //word mode
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button6.Text == "Word Mode On")
+            {
+                button6.Text = "Word Mode Off";
+                wordMode = false;
+            }
+            else
+            {
+                textBox1.Text = null;
+                button6.Text = "Word Mode On";
+                wordMode = true;
+            }
+            textBox1.Focus();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            wordLenght = Convert.ToInt32(comboBox1.Text);
+        }
+
+    }
+}
